8d85927a7b300de28ddb2dc04b874fc0f55c6eb8
Aditya <pansareaditya281@gmail.com>
2025-05-28 03:05:59 +0530

    vehicle panel completed

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import 'remixicon/fonts/remixicon.css'
+
+const LocationSearchPanel = () => {
+    return(
+        //this is the sample data
+        <div>
+            <div className="gap-4 flex items-center border-2 p-3 rounded-xl border-gray-50 active:border-black justify-start my-2">
+                <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+                <h4 className="font-medium">24B, Near Kapoor's Cafe, Sheryians Coding School, Bhopal</h4>
+            </div>
+
+            <div className="gap-4 flex items-center border-2 p-3 rounded-xl border-gray-50 active:border-black justify-start my-2">
+                <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+                <h4 className="font-medium">24B, Near Kapoor's Cafe, Sheryians Coding School, Bhopal</h4>
+            </div>
+
+            <div className="gap-4 flex items-center justify-start my-2 border-2 p-3 rounded-xl border-gray-50 active:border-black">
+                <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+                <h4 className="font-medium">24B, Near Kapoor's Cafe, Sheryians Coding School, Bhopal</h4>
+            </div>
+
+            <div className="gap-4 flex items-center justify-start my-2 border-2 p-3 rounded-xl border-gray-50 active:border-black">
+                <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+                <h4 className="font-medium">24B, Near Kapoor's Cafe, Sheryians Coding School, Bhopal</h4>
+            </div>
+
+            <div className="gap-4 flex items-center justify-start my-2 border-2 p-3 rounded-xl border-gray-50 active:border-black">
+                <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+                <h4 className="font-medium">24B, Near Kapoor's Cafe, Sheryians Coding School, Bhopal</h4>
+            </div>
+        </div>
+    ) 
+};
+
+export default LocationSearchPanel;
